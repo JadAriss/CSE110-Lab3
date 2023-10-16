d23378493568b238221c177491480da25607bf76
Ben Stirling <abenstirling@pm.me>
2023-10-16 13:46:11 -0700

    Adding branch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import javax.sound.midi.Synthesizer;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        display("This is my display");
+    }
+
+    public static void display(String message) {
+        int length = message.length();
+
+        String boarder = "+";
+
+        for (int i = 0; i < length + 2; i++) {
+            boarder += "-";
+        }
+
+        boarder += "+";
+
+        System.out.println(boarder);
+        System.out.println("| " + message + " |");
+        System.out.println(boarder);
+
     }
 }
